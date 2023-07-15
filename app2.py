8a91b467d3670a3f1dfe54c192d769c5f1a021b8
Jaime Reinoso <jaimereinoso@iMac-de-Jaime.local>
2023-07-14 21:24:31 -0500

    0.0.0.0 en vez de 127.0.0.1

--- app2.py
+++ app2.py
@@ -168,4 +168,4 @@ async def create_file(miArchivo: Annotated[bytes, File()]):
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
